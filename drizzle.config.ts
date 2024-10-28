bd0f29f8be7c69811de3056004d73e954d03d61d
CrazyStevenz <github.ekta@aleeas.com>
2024-10-28 20:36:19 +0200

    Stop prefixing db tables with the project name

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,4 @@ export default {
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["optinurse_*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -16,7 +16,7 @@ import { type AdapterAccount } from "next-auth/adapters";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `optinurse_${name}`);
+export const createTable = pgTableCreator((name) => name);
 
 export const posts = createTable(
   "post",
