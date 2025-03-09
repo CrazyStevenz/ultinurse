05ef10315fed441d68aa04856e31314ab5f3577d
CrazyStevenz <github.ekta@aleeas.com>
2025-03-09 23:40:45 +0200

    Improve solution for map not covering bottom nav

--- src/app/_components/nav/bottom-nav.tsx
+++ src/app/_components/nav/bottom-nav.tsx
@@ -5,8 +5,7 @@ export default async function BottomNav() {
 	const session = await auth();
 
 	return (
-		// Set the z-index to 1000 because the map's 400 z-index covers it
-		<div className="fixed bottom-0 left-0 z-[1000] block h-16 w-full bg-white shadow-[-20px_20px_10px_25px_rgba(0,0,0,0.75)] dark:bg-black">
+		<div className="fixed bottom-0 left-0 z-50 block h-16 w-full bg-white shadow-[-20px_20px_10px_25px_rgba(0,0,0,0.75)] dark:bg-black">
 			<div className="mx-auto grid h-full max-w-lg grid-cols-5 font-medium">
 				<BottomNavLinks isLoggedIn={Boolean(session)} />
 			</div>
