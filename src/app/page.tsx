08b96fb442ea0f846c23adc447df57368e2b7c59
CrazyStevenz <github.ekta@aleeas.com>
2025-05-04 20:11:11 +0300

    Add some welcome text to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center gap-12 px-4 py-16">
-			<h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+			<div className="text-center text-3xl font-semibold md:w-3/4 md:text-5xl">
+				Welcome to the nursing staff management with optimization algorithms
+				platform
+			</div>
+			<h1 className="text-6xl font-extrabold tracking-tight md:text-8xl">
 				Ulti<span className="text-green-500">Nurse</span>
 			</h1>
+			<div className="text-center text-xl font-light md:text-3xl xl:w-2/3">
+				Optimize the management of nursing staff, achieve efficient allocation
+				based on patient needs and nurse availability, and contribute to
+				improving the quality of medical services while reducing pressure on
+				staff.
+			</div>
 		</div>
 	);
 }
